389922066a44d0f491ece0a00ea9b5344fb3f387
Mehdi Abdellaoui <mehdi.abdellaoui2000@gmail.com>
2023-12-10 14:00:11 +0100

    Add Youtube links to inference_pipeline and app.py

--- Space/app.py
+++ Space/app.py
@@ -1,21 +1,34 @@
 from transformers import pipeline
 import gradio as gr
+from pytube import YouTube
 
 transcription_pipe = pipeline(model="explorall/whisper-small-sv-dropout-6mb")  
 translation_pipe = pipeline(model="Helsinki-NLP/opus-mt-sv-en")
 
-
 def transcribe_and_translate(audio):
     transcription = transcription_pipe(audio)["text"]
     translation = translation_pipe(transcription)[0]['translation_text']
+    
     return transcription, translation
 
-iface = gr.Interface(
-    fn=transcribe_and_translate,
-    inputs=gr.Audio(source="microphone", type="filepath"),
-    outputs=[gr.Textbox(), gr.Textbox()],
-    title="Whisper Small Swedish to English Translator",
-    description="Realtime demo for Swedish speech recognition using a fine-tuned Whisper small model, and Swedish to English translation using a small T5 model",
-)
+def transcribe_and_translate_yt(link):
+    yt = YouTube(link)
+    audio = yt.streams.filter(only_audio=True).first().download()
 
-iface.launch()
+    return transcribe_and_translate(audio)
+    
+with gr.Blocks() as demo:
+    with gr.Tab("Real-time Swedish to English Transcription and Translation"):
+        audio = gr.Audio(source="microphone", type="filepath")
+        rt_outputs = [gr.Textbox(), gr.Textbox()]
+        rt_button = gr.Button('Transcribe and Translate')
+
+    with gr.Tab("Youtube Video Transcription and Translation"):
+        link = gr.Textbox(label="Enter YouTube Video Link")
+        yt_outputs = [gr.Textbox(), gr.Textbox()]
+        yt_button = gr.Button('Transcribe and Translate YouTube Video')
+
+    rt_button.click(transcribe_and_translate, inputs=audio, outputs=rt_outputs)
+    yt_button.click(transcribe_and_translate_yt, inputs=link, outputs=yt_outputs)
+        
+demo.launch(debug=True)
