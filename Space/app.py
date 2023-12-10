4cb63229159afc4a3822de7917dfd48762c8d617
Mehdi Abdellaoui <mehdi.abdellaoui2000@gmail.com>
2023-12-10 11:32:49 +0100

    Inference pipeline, update of app.py for translation model

--- Space/app.py
+++ Space/app.py
@@ -1,14 +1,16 @@
 from transformers import pipeline
 import gradio as gr
 
-pipe = pipeline(model="explorall/whisper-small-sv")  # change to "your-username/the-name-you-picked"
+transcribe = pipeline(model="Scalable-ML/whisper-small-sv")
+translate = pipeline(model="Scalable-ML/t5-small")
 
-def transcribe(audio):
-    text = pipe(audio)["text"]
-    return text
+def transcribe_and_translate(audio):
+    text = transcribe(audio)["text"]
+    translation = translate(text)
+    return text, translation
 
 iface = gr.Interface(
-    fn=transcribe,
+    fn=transcribe_and_translate,
     inputs=gr.Audio(source="microphone", type="filepath"),
     outputs="text",
     title="Whisper Small Swedish",
