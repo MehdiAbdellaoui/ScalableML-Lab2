dd626aaa05ab38b0e92e89d385790b98ad196e70
Mehdi Abdellaoui <mehdi.abdellaoui2000@gmail.com>
2023-12-10 12:58:40 +0100

    Swedish to english translation OK

--- Space/app.py
+++ Space/app.py
@@ -1,20 +1,21 @@
 from transformers import pipeline
 import gradio as gr
 
-transcribe = pipeline(model="Scalable-ML/whisper-small-sv")
-translate = pipeline(model="Scalable-ML/t5-small")
+transcription_pipe = pipeline(model="explorall/whisper-small-sv-dropout-6mb")  
+translation_pipe = pipeline(model="Helsinki-NLP/opus-mt-sv-en")
+
 
 def transcribe_and_translate(audio):
-    text = transcribe(audio)["text"]
-    translation = translate(text)
-    return text, translation
+    transcription = transcription_pipe(audio)["text"]
+    translation = translation_pipe(transcription)[0]['translation_text']
+    return transcription, translation
 
 iface = gr.Interface(
     fn=transcribe_and_translate,
     inputs=gr.Audio(source="microphone", type="filepath"),
-    outputs="text",
-    title="Whisper Small Swedish",
-    description="Realtime demo for Swedish speech recognition using a fine-tuned Whisper small model.",
+    outputs=[gr.Textbox(), gr.Textbox()],
+    title="Whisper Small Swedish to English Translator",
+    description="Realtime demo for Swedish speech recognition using a fine-tuned Whisper small model, and Swedish to English translation using a small T5 model",
 )
 
 iface.launch()
